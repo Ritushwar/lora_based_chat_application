43a6efe66c146967de82df11b8135fc3d5e7599f
Ritushwar <ritushwarneupane111@gmail.com>
2026-02-17 08:19:02 +0545

    manage lora communication

---
+++ lib/lora/lora_setup.cpp
@@ -0,0 +1,109 @@
+#include<lora_setup.h>
+#include<my_protocal.h>
+// LoRa Modes
+#define RX_MODE 0
+#define TX_MODE 1
+int mode = RX_MODE;
+
+void setup_lora_communication(){
+  // setup LoRa
+  LoRa.setPins(cs, reset, dio0);
+
+  pinMode(led_when_receive, OUTPUT);
+  pinMode(led_on_r_mode, OUTPUT);
+  pinMode(led_on_t_mode, OUTPUT);
+
+  if (!LoRa.begin(frequency)){
+    Serial.println("LoRa init failed");
+    while (1);
+    }
+  setMode(RX_MODE);          // by default
+  Serial.println("LoRa ready to operate");
+};
+
+void setMode(int stataus){
+    if(stataus== RX_MODE){
+      LoRa.receive();                       // set to receive mode
+      digitalWrite(led_on_t_mode, LOW);
+      digitalWrite(led_on_r_mode, HIGH);    // indicate my led
+    }
+    else{
+      digitalWrite(led_on_r_mode, LOW);      // set to transmit mode indicate my led
+      digitalWrite(led_on_t_mode, HIGH);      
+    }
+};
+
+void sendRTS(uint8_t * header, int header_size){
+  // set to transmitting mode
+  mode = TX_MODE;
+  setMode(mode);
+  delay(20);
+
+  Serial.println("Sending RTS.....");
+  LoRa.beginPacket();
+  LoRa.write(header, header_size);
+  LoRa.endPacket();
+  Serial.println("RTS has been sent");
+
+  mode = RX_MODE;
+  setMode(mode);
+  delay(200);
+}
+
+void receivePKT(int header_size, int pay_pos){
+  if(mode == RX_MODE){
+    int packetSize = LoRa.parsePacket();
+    if(packetSize >= header_size){
+      Serial.println("Packet is received");
+      uint8_t rxHeader[header_size];
+      LoRa.readBytes(rxHeader, header_size);
+      Serial.print("Header is received");
+      // Serial.println(rxHeader.c_str());
+      // can blink led to indicate that's optional
+      int payload_len = rxHeader[pay_pos];
+      // if payload is available
+      if(payload_len>0 && packetSize >= header_size + payload_len){
+        uint8_t payload[payload_len];
+        LoRa.readBytes(payload, payload_len);
+
+        // merge header + payload
+        uint8_t merged[header_size + payload_len];
+        memcpy(merged, rxHeader, header_size);
+        memcpy(merged+header_size, payload, payload_len);
+
+        protocol_handlePacket(merged, header_size + payload_len);
+        return;
+                
+      }
+      // pass to protocol to further process
+      protocol_handlePacket(rxHeader, header_size);
+    }
+  }
+}
+
+void sendDATA(uint8_t * header, uint8_t * payload){
+  mode = TX_MODE;
+  setMode(mode);
+
+  // send header and payload at once
+  LoRa.beginPacket();
+  LoRa.write(header, 10);      // header size is fixed
+  LoRa.write(payload, globalData.my_msg.length());
+  LoRa.endPacket();
+
+  mode = RX_MODE;
+  setMode(mode);
+}
+
+void sendPKT(uint8_t * header){
+  Serial.println("Sending CTS");
+  mode = TX_MODE;
+  setMode(mode);
+  LoRa.beginPacket();
+  LoRa.write(header, 10); 
+  LoRa.endPacket();
+  Serial.println("CTS is sent");
+  mode = RX_MODE;
+  setMode(mode);
+  delay(10);
+}
---
+++ lib/lora/lora_setup.h
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <LoRa.h>
+#include<global_data.h>
+
+// LoRa Frequency
+const long frequency = 433E6;
+
+// ESP32 ↔ LoRa pin mapping
+const int cs    = 5;   // NSS
+const int reset = 2;   // RESET
+const int dio0  = 22;   // DIO0 (RX interrupt)
+
+const int led_when_receive = 13;  // Blinks on packet receive
+const int led_on_r_mode    = 12;  // ON = receiver mode
+const int led_on_t_mode    = 14;  // ON = transmitting mode
+
+void setup_lora_communication();
+void setMode(int stataus);
+void sendRTS(uint8_t *, int);
+void receivePKT(int, int);
+void sendDATA(uint8_t *,  uint8_t *);
+void sendPKT(uint8_t *);
