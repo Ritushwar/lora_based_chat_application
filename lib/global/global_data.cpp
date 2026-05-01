9f04a7f14c2c1319e45e26a5da43aab61abe1b00
Ritushwar <ritushwarneupane111@gmail.com>
2026-05-01 23:04:18 +0545

    global_data

--- lib/global/global_data.cpp
+++ lib/global/global_data.cpp
@@ -1,2 +1,2 @@
-#include<global_data.h>
+#include "global_data.h"
 GlobalData globalData;
--- lib/global/global_data.h
+++ lib/global/global_data.h
@@ -1,26 +1,18 @@
-#ifndef GLOBAL_DATA_H
-#define GLOBAL_DATA_H
+#pragma once
+#include <string>
 
-#include <stdint.h>
-#include <string.h>
-#include <Arduino.h>
-
-struct  GlobalData
-{
+struct GlobalData {
     bool deviceConnected = false;
-    bool notifyEnabled = false;
-    u_int8_t payload_len = 0x00;
-    std::string ble_name;
-    std::string my_msg;
-    std::string received_msg;
-    uint8_t NODE_ID   = 0x01;
-    uint8_t USER_ID   = 0x01;
-    uint8_t node_id_r = 0x02;
-    uint8_t user_id_r = 0x02;
+    bool notifyEnabled   = false;
+
+    int chip_s;
+    int reset;
+    int intr;
+    int led_on_t_mode;
+    int led_on_r_mode;
     
+    std::string ble_name = "";
+    std::string received_msg = "";   // filled by LoRa FSM, read by BLE notify
 };
 
 extern GlobalData globalData;
-
-
-#endif
