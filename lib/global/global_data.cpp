1112f1bd214df9946f963599995b662e9e1bd940
Ritushwar <ritushwarneupane111@gmail.com>
2026-05-04 18:15:07 +0545

    changes global data

--- lib/global/global_data.cpp
+++ lib/global/global_data.cpp
@@ -1,2 +1,6 @@
 #include "global_data.h"
+void set_node_id(uint8_t sender,uint8_t receiver){
+    globalData.n_id_s = sender;
+    globalData.n_id_r = receiver;
+}
 GlobalData globalData;
--- lib/global/global_data.h
+++ lib/global/global_data.h
@@ -11,8 +11,15 @@ struct GlobalData {
     int led_on_t_mode;
     int led_on_r_mode;
     
+    uint8_t u_id_s;
+    uint8_t n_id_s;
+
+    uint8_t u_id_r;
+    uint8_t n_id_r;
     std::string ble_name = "";
-    std::string received_msg = "";   // filled by LoRa FSM, read by BLE notify
+    bool notification_flag = false;
+    std::string received_msg = "";   // filled by handle json after sending fom ble
 };
 
+void set_node_id(uint8_t,uint8_t);
 extern GlobalData globalData;
